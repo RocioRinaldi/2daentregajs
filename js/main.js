f81de9fac1602a7046bc59a610f86e660488afbb
rociorinaldi <rociorinaldi16@gmail.com>
2024-08-06 13:35:14 -0300

    carga inicial

---
+++ js/main.js
@@ -0,0 +1,97 @@
+const carrito = [];
+
+const productos = [
+    {
+        id: "reloj-01",
+        titulo: "Reloj 01",
+        precio: 4000,
+        img: "img/01.png",
+    },
+    {
+        id: "reloj-02",
+        titulo: "Reloj 02",
+        precio: 4000,
+        img: "img/02.png",
+    },
+    {
+        id: "reloj-03",
+        titulo: "Reloj 03",
+        precio: 4000,
+        img: "img/03.png",
+    }
+];
+
+const contenedorProductos = document.querySelector("#productos");
+const carritoVacio = document.querySelector("#carrito-vacio");
+const carritoProductos = document.querySelector("#carrito-productos");
+const carritoTotal = document.querySelector("#carrito-total");
+
+
+
+productos.forEach((producto) => {
+
+    let div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+        <img class="producto-img" src="${producto.img}" alt="">
+        <h3>${producto.titulo}</h3>
+        <p>$${producto.precio}</p>
+    `;
+
+    let button = document.createElement("button");
+    button.classList.add("producto-btn");
+    button.innerText = "Agregar al carrito";
+    button.addEventListener("click", () => {
+        agregarAlCarrito(producto);
+    })
+
+    div.append(button);
+    contenedorProductos.append(div);
+});
+
+const agregarAlCarrito = (producto) => {
+    carrito.push(producto);
+    actualizarCarrito();
+}
+
+function actualizarCarrito() {
+    if (carrito.length === 0) {
+        carritoVacio.classList.remove("d-none");
+        carritoProductos.classList.add("d-none");
+    } else {
+        carritoVacio.classList.add("d-none");
+        carritoProductos.classList.remove("d-none");
+
+        carritoProductos.innerHTML = "";
+        carrito.forEach((producto) => {
+            let div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <h3>${producto.titulo}</h3>
+                <p>$${producto.precio}</p>
+            `;
+
+            let button = document.createElement("button");
+            button.classList.add("carrito-producto-btn");
+            button.innerText = "✖️";
+            button.addEventListener("click", () => {
+                borrarDelCarrito(producto);
+            })
+
+            div.append(button);
+            carritoProductos.append(div);
+        })
+    }
+    actualizarTotal();
+}
+
+function borrarDelCarrito(producto) {
+    const indice = carrito.findIndex((item) => item.id === producto.id);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+}
+
+function actualizarTotal() {
+    const total = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+    carritoTotal.innerText = "$" + total;
+}
